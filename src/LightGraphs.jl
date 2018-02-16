8a3e7eef346cdeb17c5f08ebd08f34a9a80f9c91
SohamTamba <soham001995@gmail.com>
2018-02-16 13:26:29 +0530

    Create testbench script

--- src/LightGraphs.jl
+++ src/LightGraphs.jl
@@ -120,7 +120,7 @@ smallgraph,
 euclidean_graph,
 
 #minimum_spanning_trees
-kruskal_mst, prim_mst,
+kruskal_mst, prim_mst, kruskal_mst_old,
 
 #biconnectivity and articulation points
 articulation, biconnected_components,
